848869a0609718aeb7a2407ec8184a09b507353f
aleal76 <aleal76@hotmail.com>
2020-01-11 20:13:54 -0300

    inicios nextaa

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+      <div>
+        <p>This is the about page by AA the best</p>
+      </div>
+    );
+  }
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+// This is the Link API
+import Link from 'next/link';
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+    <p>Hello Next.js </p>
+    <p>by AA the very Best</p>
+    </p>
+  </div>
+);
+
+export default Index;
